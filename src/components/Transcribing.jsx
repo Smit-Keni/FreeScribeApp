089d84e85b8743f7d619dc18ade3f6e099757556
Smit-Keni <smitkeni45@gmail.com>
2024-07-25 15:42:49 +0530

    fourth commit

--- src/components/Transcribing.jsx
+++ src/components/Transcribing.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 export default function Transcribing(props) {
     const {downloading} = props
   return (
-    <div className='fex item-center flex-col 
-    justify-center gap-10 md:gap-14 py-24'>
+    <div className='flex item-center flex-col 
+        justify-center text-center gap-10 md:gap-14 pb-24 p-4 flex-1'>
         <div className='flex flex-col gap-2 sm:gap-4'>
             <h1 className='font-semibold text-4xl sm:text-5xl 
             md:text-6xl'><span className='text-blue-400 bold'>
@@ -12,13 +12,13 @@ export default function Transcribing(props) {
             </span>
 
             </h1>
-            <p>{!downloading? 'warming up cylcinders':
+            <p>{!downloading? 'warming up cylinders':
             'core cylinders engaged'}</p>
-            <div className='flex flex-col gap-2 sm:gap-4 max-2-[500px] 
+            <div className='flex flex-col gap-2 sm:gap-4 max-w-[400px] 
             mx-auto w-full'>
                 {[0,1,2].map(val=>{
                     return(
-                        <div key={val} className={'rounded-full h-2 sm-h-3 bg-slate-400 loading' +
+                        <div key={val} className={'rounded-full h-2 sm:h-3 bg-slate-400 loading' +
                             `loading${val}`}></div>
                         )
                         }
